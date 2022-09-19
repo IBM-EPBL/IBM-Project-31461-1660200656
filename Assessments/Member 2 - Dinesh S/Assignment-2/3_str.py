ce0e8df9817f65a9346e066e704a7c3eae17fee6
dinesh-2807 <dineshsaravanan28072002@gmail.com>
2022-09-19 14:19:08 -0700

    added my assignment files

---
+++ Assessments/Member 2 - Dinesh S/Assignment-2/3_str.py
@@ -0,0 +1,18 @@
+# let the string be
+
+string = "The quick brown fox jumps over the lazy"
+
+#task 1
+# concatanation
+modified_string = string + " dog."
+print(modified_string)
+
+#task 2
+# reverse the string
+reversed_string = string[::-1]
+print(reversed_string)
+
+# task 3
+# slice the string
+sliced_string = string[4:16:1]
+print(sliced_string)
