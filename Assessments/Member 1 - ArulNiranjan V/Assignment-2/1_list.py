c8516478625ad09056e4dd10cb42f87778a3dd2b
Arulniranjan.V <111488089+Hyper-2002@users.noreply.github.com>
2022-09-19 22:15:57 -0700

    Added My assignment Files

---
+++ Assessments/Member 1 - ArulNiranjan V/Assignment-2/1_list.py
@@ -0,0 +1,22 @@
+numbers_list = [34,56,367,78,577,12,567,23,899,599,678,321]
+
+# insert interger at postion
+numbers_list.insert(3,514)
+
+#print the list
+print(numbers_list) 
+
+# delete the first occurrence of the integer
+numbers_list.remove(845)
+
+#append integer at end of the list
+numbers_list.append(375)
+
+# sort the list
+numbers_list.sort()
+
+#pop the last element from the list
+numbers_list.pop()
+
+# reverse the list 
+numbers_list.reverse()
